bf5e723718b00cd80aed84c7008dcdd930839604
Marcel Goethals <marcel.goethals@gmail.com>
2017-05-01 23:37:27 +0200

    docker

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s^__PORT__^$PORT^g" /app/src/config.js
-sed -i "s^__DB_STORAGE__^$DB_STORAGE^g" /app/src/config.js
+sed -i "s^__PORT__^$PORT^g" /app/config.js
+sed -i "s^__DB_STORAGE__^$DB_STORAGE^g" /app/config.js
 
 cd /app
 npm start
