f518db778c363f5422b24b55b651beafddf05428
Marcel Goethals <marcel.goethals@gmail.com>
2017-07-14 18:20:07 +0200

    More Advanced search

--- src/routes/search.js
+++ src/routes/search.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 
 	getAllEfforts: function(request, reply) {
-		reply(model.Effort.findAll({
+		model.Effort.findAll({
 			where: {
 				$or: {
 					name: {like: "%"+request.payload.query+"%"},
@@ -47,6 +47,52 @@ module.exports = {
 					]
 				}
 			]
-		}));
+		}).then(function(efforts){
+			model.Task.findAll({
+				where: {
+					$or: {
+						name: {like: "%"+request.payload.query+"%"},
+						means: {like: "%"+request.payload.query+"%"}
+					}
+				},
+				attributes: ['id', 'name', 'means'],
+				limit: 20,
+				include: [
+					{
+						model: model.Program,
+						attributes: ['id', 'name'],
+						through: {attributes: []},
+					}
+				]
+			}).then(function(tasks){
+
+				efforts = efforts.filter(function(result){
+					return (result.Tasks[0] && result.Tasks[0].Programs[0])?true:false;
+				}).map(function(result){
+					result = result.get({plain: true});
+					result.type = "effort";
+					result.task = result.Tasks[0];
+					result.program = result.Tasks[0].Programs[0];
+					delete result.Tasks;
+					delete result.task.Programs;
+					delete result.Programs;
+					return result;
+				});
+
+				tasks = tasks.filter(function(result){
+					return (result.Programs[0])?true:false;
+				}).map(function(result){
+					result = result.get({plain: true});
+					result.type = "task";
+					result.program = result.Programs[0];
+					delete result.Programs;
+					return result;
+				});
+
+
+				reply(efforts.concat(tasks));
+			});
+		});
+
 	}
 };
